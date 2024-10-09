64f348875805fe7fc65a87592be268ba9c486ee2
cwclaib <cwclaib@gmail.com>
2024-10-09 16:46:52 -0400

    scorigami test

---
+++ football/scorigami/script.js
@@ -0,0 +1,14 @@
+const url = ("https://docs.google.com/spreadsheets/d/e/2PACX-1vQ77Ih8-pz10Wty_e9FmiwJ95Zafv_025G_ZSfiQ-0IFjP3kAZ4rZHVEiY4DU49CNUiZMlyJSZ9O7rq/pub?gid=1191433987&single=true&output=csv";
+
+let results;
+
+const csvData = Papa.parse, (url, {
+	dyanmicTypig: true,
+	download: true,
+	header: true,
+	comments: "*=",
+	complete: function(data) {
+        result = data.data
+	}
+});
+
