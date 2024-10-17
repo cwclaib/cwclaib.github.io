fa594931601e0fa9d89f2d7258111227a38894b1
cwclaib <cwclaib@gmail.com>
2024-10-16 20:32:45 -0400

    durn not initializing things... realized b was dumb anyways so he gone

--- football/scorigami/script.js
+++ football/scorigami/script.js
@@ -86,10 +86,8 @@ function setDefaultColors(table, scores) {
 				elem.classList.add("orange");
 			}
 			if ( i < 6 ) {
-				let b = 0;
-				if ( col === impossibleScore[i][b]) {
-					let b = b + 1;
-					if ( row === impssibleScore[i][b] ) {
+				if ( col === impossibleScore[i][0]) {
+					if ( row === impssibleScore[i][1] ) {
 						elem.classList.add("black");
 						let i = i + 1;
 					}
